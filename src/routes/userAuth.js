5f09570a84d14b1404dca28ff2f17e465634486b
Aneudy Adames <a4aneudy@gmail.com>
2019-01-03 02:06:09 -0500

    added async/await to the register and login routes to avoid .then

--- src/routes/userAuth.js
+++ src/routes/userAuth.js
@@ -6,34 +6,51 @@ const router = express.Router();
 
 router.get("/test", (req, res) => res.json({ login: "required" }));
 
-// route    api/user/resgister
+// route    api/users/register
 // method  POST
 // access  Public
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
-  User.findOne({ email })
-    .then(user => {
-      if (user) {
-        // user found in database
-        res.status(400).json({ msg: "User already exist" });
-      } else {
-        const newUser = new User({
-          name,
-          email,
-          password
-        });
+  const user = await User.findOne({ email });
+  if (user) {
+    // user found in database
+    res.status(400).json({ msg: "User already exist" });
+  } else {
+    // creating new User
+    const newUser = new User({
+      name,
+      email,
+      password
+    });
+    // encrypting password
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        newUser.password = hash;
+        newUser.save();
+        return res.status(200).json({ msg: "user Created" });
+      });
+    });
+  }
+});
 
-        // encrypting password
-        bcrypt.genSalt(10, (err, salt) => {
-          bcrypt.hash(newUser.password, salt, (err, hash) => {
-            newUser.password = hash;
-            newUser.save();
-            res.status(200).json({ msg: "user Created" });
-          });
-        });
-      }
-    })
-    .catch(err => console.log(err));
+// route    api/users/login
+// method  post
+// access  Public
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  let user = await User.findOne({ email });
+
+  // checks for user in database
+  if (!user) {
+    return res.status(404).json({ msg: "user not found" });
+  }
+  //  compare password typed to the DB
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (isMatch) {
+    res.json({ msg: "logged in" });
+  } else {
+    return res.status(404).json({ msg: "password incorrect" });
+  }
 });
 
 module.exports = router;
